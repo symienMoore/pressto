a225fa7c2d81b1447344008bd403ee577c5c9ec4
symi <symien.moore@gmail.com>
2024-12-10 23:40:30 -0500

    initial commit

---
+++ src/main/java/org/lightwave/pressto/PresstoApplication.java
@@ -0,0 +1,13 @@
+package org.lightwave.pressto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresstoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresstoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lightwave/pressto/PresstoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lightwave.pressto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresstoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
